de827a5869b99cea19c024eba1a5a2f0d0034d2e
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-11-18 01:14:05 -0500

    adding files

---
+++ build/precache-manifest.31f317c0e9172bdf0718b8c8c85bd3f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d70951e08d959d3631142931823ea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eafb9e89b9c90a808e9",
+    "url": "/static/css/main.bcecb733.chunk.css"
+  },
+  {
+    "revision": "222a1eb4288c197a2af1",
+    "url": "/static/js/2.abb722af.chunk.js"
+  },
+  {
+    "revision": "5eafb9e89b9c90a808e9",
+    "url": "/static/js/main.45d9bf90.chunk.js"
+  },
+  {
+    "revision": "927d2e1dc6802baa6015",
+    "url": "/static/js/runtime-main.fecc3a72.js"
+  },
+  {
+    "revision": "11bcd1e1f6ea8dc5556ca166783ecfbe",
+    "url": "/static/media/dog.11bcd1e1.png"
+  },
+  {
+    "revision": "47d6e6bf5dac4dc5423e0e397ff13882",
+    "url": "/static/media/github.47d6e6bf.png"
+  },
+  {
+    "revision": "28052b5572a9e909735535b13319f3bf",
+    "url": "/static/media/kid.28052b55.png"
+  },
+  {
+    "revision": "b188a82b79ca3034482df26dc2a053d7",
+    "url": "/static/media/ring1.b188a82b.png"
+  },
+  {
+    "revision": "0485eade7e966a2b534cf4093a9635ee",
+    "url": "/static/media/ring2.0485eade.png"
+  }
+]);
