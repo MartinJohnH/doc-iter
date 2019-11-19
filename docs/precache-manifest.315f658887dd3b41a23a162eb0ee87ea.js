35d0af9f7a7c20e1d5fc66e0b37d7dffac41595e
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-11-18 22:04:38 -0500

    changed font for body text

---
+++ docs/precache-manifest.315f658887dd3b41a23a162eb0ee87ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6aa04a60a090f60ed3df78b2ab84cd",
+    "url": "/doc-iter/index.html"
+  },
+  {
+    "revision": "5718cc708a02b0ed3e5e",
+    "url": "/doc-iter/static/css/main.c4551b6d.chunk.css"
+  },
+  {
+    "revision": "3d888d38b017f33f62d7",
+    "url": "/doc-iter/static/js/2.9ca70cf7.chunk.js"
+  },
+  {
+    "revision": "5718cc708a02b0ed3e5e",
+    "url": "/doc-iter/static/js/main.d5f41c58.chunk.js"
+  },
+  {
+    "revision": "c0d51a846a6cf9cdae17",
+    "url": "/doc-iter/static/js/runtime-main.e4656aa5.js"
+  },
+  {
+    "revision": "11bcd1e1f6ea8dc5556ca166783ecfbe",
+    "url": "/doc-iter/static/media/dog.11bcd1e1.png"
+  },
+  {
+    "revision": "47d6e6bf5dac4dc5423e0e397ff13882",
+    "url": "/doc-iter/static/media/github.47d6e6bf.png"
+  },
+  {
+    "revision": "28052b5572a9e909735535b13319f3bf",
+    "url": "/doc-iter/static/media/kid.28052b55.png"
+  },
+  {
+    "revision": "b188a82b79ca3034482df26dc2a053d7",
+    "url": "/doc-iter/static/media/ring1.b188a82b.png"
+  },
+  {
+    "revision": "0485eade7e966a2b534cf4093a9635ee",
+    "url": "/doc-iter/static/media/ring2.0485eade.png"
+  }
+]);
